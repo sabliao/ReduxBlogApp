2fa13ea1baa8929c02cb94a7ac0acfe66f4f6b82
sabliao <sabrinagliao@gmail.com>
2017-09-22 22:38:13 -0700

    Add basic form to posts new page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return(
-            <div>
-                Posts New
-            </div>
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+function validate(values) {
+    const errors;
+
+    // Validate the inputs from 'values'
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content";
+    }
+    // If errors is empty, form is fine to submit
+    // If errors has *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm',
+    }
+})(PostsNew);
